02c0eccc35671f35d2e2e00cf969740ebc3255b9
MichalPC1 <banka.michal1964@gmail.com>
2018-12-12 21:17:59 +0100

    final version of project, source cleared

--- Source.cpp
+++ Source.cpp
@@ -6,35 +6,7 @@ int main()
 {
 	srand(time(nullptr));
 
-	//menu().mainMenu();
-
-	std::cout << "=============== 17 (39) ====================" << std::endl;
-	matrix graph("17.txt");
-
-	std::vector<int> minCycle = graph.simulatedAnnealingInit();
-	graph.printCycle(minCycle);
-	std::cout << graph.distance2(minCycle) << std::endl;
-
-	std::cout << "=============== 56 (1608) ====================" << std::endl;
-	graph.fillFromFile("56.txt");
-
-	minCycle = graph.simulatedAnnealingInit();
-	graph.printCycle(minCycle);
-	std::cout << graph.distance2(minCycle) << std::endl;
-
-	std::cout << "=============== 70 (38673) ====================" << std::endl;
-	graph.fillFromFile("70.txt");
-
-	minCycle = graph.simulatedAnnealingInit();
-	graph.printCycle(minCycle);
-	std::cout << graph.distance2(minCycle) << std::endl;
-
-	std::cout << "=============== 171 (2755) ====================" << std::endl;
-	graph.fillFromFile("171.txt");
-
-	minCycle = graph.simulatedAnnealingInit();
-	graph.printCycle(minCycle);
-	std::cout << graph.distance2(minCycle) << std::endl;
+	menu().mainMenu2();
 
 	std::cin.get();
 	return 0;
