bcb7356dfd06488a34940f48fb3166290a67524e
MichalPC1 <banka.michal1964@gmail.com>
2019-01-07 02:02:28 +0100

    source to test GA

--- Source.cpp
+++ Source.cpp
@@ -6,8 +6,20 @@ int main()
 {
 	srand(time(nullptr));
 
-	menu().mainMenu2();
+	//menu().mainMenu2();
 
+	matrix matrix;
+	matrix.changeSizeAndClear(10);
+	for (int i = 0; i < 10; ++i)
+	{
+		matrix.fillVertexConnectionsRandom(i, 0, 20);
+	}
+
+	std::vector<int> min = matrix.geneticAlgorithmInit();
+	std::vector<int> min2 = matrix.branchAndBoundInit();
+
+	matrix.printCycle(min);
+	matrix.printCycle(min2);
 	std::cin.get();
 	return 0;
 }
