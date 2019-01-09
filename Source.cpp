026c3ad93f7d9a5307938fce6e54e2ee879816c0
MichalPC1 <banka.michal1964@gmail.com>
2019-01-09 04:19:18 +0100

    Main use menu class now

--- Source.cpp
+++ Source.cpp
@@ -7,25 +7,34 @@ int main()
 {
 	srand(time(nullptr));
 
-	//menu().mainMenu2();
+	menu().mainMenu3();
 
-	matrix matrix;
-	matrix.changeSizeAndClear(150);
-	for (int i = 0; i < 150; ++i)
-	{
-		matrix.fillVertexConnectionsRandom(i, 0, 20);
-	}
+	/*matrix matrix("56.txt");
+	
 	matrix.show();
+
+	int dist1 = 0, dist2 = 0, dist3 = 0;
+
 	for (int i = 0; i < 20; ++i)
 	{
-		std::vector<int> min = matrix.geneticAlgorithm(50, 999, 0.1, 0.9, 10, SelectionMethod::TOP);
+		std::vector<int> min = matrix.geneticAlgorithm(50, 200, 0.9, 0.1, 10, SelectionMethod::TOP);
+		dist1 += matrix.distance(min);
 		matrix.printCycle(min);
-		std::vector<int> min2 = matrix.geneticAlgorithm(50, 999, 0.1, 0.9, 10, SelectionMethod::PROBABILITY);
+
+		std::vector<int> min2 = matrix.geneticAlgorithm(50, 200, 0.9, 0.1, 10, SelectionMethod::PROBABILITY);
+		dist2 += matrix.distance(min2);
 		matrix.printCycle(min2);
-		//std::vector<int> min3 = matrix.branchAndBoundInit();
-		//matrix.printCycle(min3);
-		std::cout << " ==========================================  " << std::endl;
+
+		std::vector<int> min3 = matrix.randomSearch(1);
+		dist3 += matrix.distance(min3);
+		matrix.printCycle(min3);
+
+		std::cout << i << ". ==========================================  " << std::endl;
 	}
+
+	std::cout << "#1: " << dist1 / 20 << std::endl;
+	std::cout << "#2: " << dist2 / 20 << std::endl;
+	std::cout << "#3: " << dist3 / 20 << std::endl;*/
 	
 	std::cin.get();
 	return 0;
